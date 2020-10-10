022833fed03e4e07098e2a3491bdaa22e30eeac2
Matthias Kubisch <kubisch@informatik.hu-berlin.de>
2020-10-10 15:27:11 +0200

    updated comments.

--- source/bakiwi_kit/blinky/main.c
+++ source/bakiwi_kit/blinky/main.c
@@ -1,17 +1,17 @@
 /*
 
-   +------------------+
+   +-------------------+
    | BAKIWI HELLOWORLD |
-   +------------------+
+   +-------------------+
 
    Toggels LED pin every 100ms
 
    0) check   : avrdude -c arduino -p t84 -P /dev/ttyUSB0 -b 19200 -v
-   1) compile : avr-gcc -mmcu=attiny85 -Os -c main.c -o main.o
+   1) compile : avr-gcc -mmcu=attiny84 -Os -c main.c -o main.o
    2) link    : avr-gcc main.o -o main.elf
    3) to hex  : avr-objcopy -O ihex -j .text -j .data main.elf main.hex
-   4) check   : avr-size --mcu=attiny85 -C main.elf
-   5) burn    : avrdude -c arduino -p t85 -P /dev/ttyUSB0 -b 19200 -v -U flash:w:main.hex:a
+   4) check   : avr-size --mcu=attiny84 -C main.elf
+   5) burn    : avrdude -c arduino -p t84 -P /dev/ttyUSB0 -b 19200 -v -U flash:w:main.hex:a
 
    or use make, flash, fuse, install
 
@@ -29,12 +29,12 @@ int main (void) {
     DDRB |= (1 << PB2); // set led right pin to output
 
     PORTA |= (1 << PA7); // set led left high (on)
-	
+
     while(1) {
-		_delay_ms(500);
-		PORTA ^= (1 << PA7); // toggle led
-		PORTB ^= (1 << PB2); // toggle led
-	}
+        _delay_ms(500);
+        PORTA ^= (1 << PA7); // toggle led
+        PORTB ^= (1 << PB2); // toggle led
+    }
 
    return 0;
 }
